51cb1e749854c44e31fac6165587583e27a1d01b
Pretro <jort2212@outlook.com>
2021-04-19 08:15:01 +0000

    Customized admin file

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Category, Product
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'rating', 'sku', 'category', 'image')
+    prepopulated_fields = {'friendly_name': ('name')}
+
+    ordering = ('sku')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('friendly_name', 'name', )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
